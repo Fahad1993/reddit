977c4b2427288acbdb582d66d717e11e6aaeb1e6
Fahad1993 <foso1015@gmail.com>
2022-10-20 07:19:53 +0300

    signup/login/logout and username selection

--- components/Post.js
+++ components/Post.js
@@ -2,7 +2,7 @@ import timeago from 'lib/timeago'
 
 export default function Post({ post }) {
     return (
-        <div className='flex flex-col mb-4 border border-3 border-black p-10 bg-gray-200 mx-20 my-10'>
+        <div className='flex flex-col mb-4 border border-3 border-black p-10 bg-gray-400 mx-20 my-10'>
             <div className='flex flex-shrink-0 pb-0 '>
                 <div className='flex-shrink-0 block group '>
                     <div className='flex items-center text-gray-800'>
