f7740617b651271b24eaa5a97b9a5893e9b48476
Fahad1993 <foso1015@gmail.com>
2022-10-21 06:15:32 +0300

    consistent header

--- pages/r/[subreddit].js
+++ pages/r/[subreddit].js
@@ -4,32 +4,36 @@ import Link from 'next/link'
 import Posts from 'components/Posts'
 
 export default function Subreddit({ subreddit, posts }) {
-  if (!subreddit) {
-    return <p className='text-center p-5'>Subreddit does not exist 😞</p>
-  }
+    if (!subreddit) {
+        return <p className='text-center p-5'>Subreddit does not exist 😞</p>
+    }
 
-  return (
-    <>
-     <Link href={`/`}>
-        <a className='text-center p-5 underline block'>
-          🔙 back to the homepage
-        </a>
-      </Link>
-      <p className='text-center p-5'>/r/{subreddit.name}</p>
-      <Posts posts={posts} />
-    </>
-  )
+    return (
+        <>
+            <header className='bg-gray-800 text-white h-12 flex pt-3 px-5 pb-2'>
+                <Link href={`/`}>
+                    <a className='underline'>Home</a>
+                </Link>
+                <p className='grow'></p>
+            </header>
+            <header className='bg-gray-800 text-white h-12 flex pt-3 px-5 pb-2'>
+                <p className='text-center'>/r/{subreddit.name}</p>
+                <p className='ml-4 text-left grow'>{subreddit.description}</p>
+            </header>
+            <Posts posts={posts} />
+        </>
+    )
 }
 
 export async function getServerSideProps({ params }) {
-  const subreddit = await getSubreddit(params.subreddit, prisma)
-  let posts = await getPostsFromSubreddit(params.subreddit, prisma)
-	posts = JSON.parse(JSON.stringify(posts))
+    const subreddit = await getSubreddit(params.subreddit, prisma)
+    let posts = await getPostsFromSubreddit(params.subreddit, prisma)
+    posts = JSON.parse(JSON.stringify(posts))
 
-  return {
-    props: {
-      subreddit,
-      posts,
-    },
-  }
+    return {
+        props: {
+            subreddit,
+            posts,
+        },
+    }
 }
