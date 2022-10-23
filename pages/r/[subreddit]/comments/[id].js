2789ae2ce7816691ea9ca38ee7133fccfe7918ad
Fahad1993 <foso1015@gmail.com>
2022-10-23 05:27:53 +0300

    Let people add comments

--- pages/r/[subreddit]/comments/[id].js
+++ pages/r/[subreddit]/comments/[id].js
@@ -2,10 +2,21 @@ import prisma from 'lib/prisma'
 import { getPost, getSubreddit } from 'lib/data.js'
 import Link from 'next/link'
 import timeago from 'lib/timeago'
+import NewComment from 'components/NewComment'
+import { useSession } from 'next-auth/react'
 
 
 export default function Post({ subreddit, post }) {
+    const { data: session, status } = useSession()
+
+    const loading = status === 'loading'
+
+    if (loading) {
+        return null
+    }
+
     if (!post) return <p className='text-center p-5'>Post does not exist 😞</p>
+
     return (
         <>
             <header className='bg-gray-800 text-white h-12 flex pt-3 px-5 pb-2'>
@@ -23,7 +34,7 @@ export default function Post({ subreddit, post }) {
 
 
 
-            <div className='flex flex-col mb-4 border border-3 border-black p-10 bg-gray-200 mx-20 my-10'>
+            <div className='flex flex-col mb-4 border border-3 border-black p-10 bg-gray-800 mx-20 my-10'>
                 <div className='flex flex-shrink-0 pb-0 '>
                     <div className='flex-shrink-0 block group '>
                         <div className='flex items-center text-gray-800'>
@@ -42,7 +53,20 @@ export default function Post({ subreddit, post }) {
                         {post.content}
                     </p>
                 </div>
+                {session ? (
+                <NewComment post={post} />
+            ) : (
+                <p className='mt-5'>
+                    <a className='mr-1 underline' href='/api/auth/signin'>
+                        Login
+                    </a>
+                    to add a comment
+                </p>
+            )}
             </div>
+
+           
+
         </>
     )
 }
